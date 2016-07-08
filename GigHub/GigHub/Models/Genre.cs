be84be55808f1df464911e0c6fb4200d8090e53c
Mateusz Klosinski <matix003@gmail.com>
2016-07-08 22:03:05 +0200

    Override a gig and genre conventions.

--- GigHub/GigHub/Models/Genre.cs
+++ GigHub/GigHub/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace GigHub.Models
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
         public string Name { get; set; }
     }
 }
