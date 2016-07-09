1f6a807135ec6c62dea8b33cbef5db5bfca927ce
Mateusz Klosinski <matix003@gmail.com>
2016-07-09 19:40:20 +0200

    Improve the usability for form.

--- GigHub/GigHub/ViewModels/FutureDate.cs
+++ GigHub/GigHub/ViewModels/FutureDate.cs
@@ -13,7 +13,7 @@ namespace GigHub.ViewModels
         {
             DateTime dateTime;
             var isValid = DateTime.TryParseExact(Convert.ToString(value),
-                "dd MMM yyyy",
+                "dd MM yyyy",
                 CultureInfo.CurrentCulture,
                 DateTimeStyles.None,
                 out dateTime
