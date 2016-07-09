6692ee692bda128ecebd5e70dcb14a5cae703a92
Mateusz Klosinski <matix003@gmail.com>
2016-07-09 23:59:27 +0200

    Little changes in database and adding a simple Web Api.

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigID)
+        {
+            var attend = new Attendance()
+            {
+                GigId = gigID,
+                AttendeeId = User.Identity.GetUserId(),
+            };
+
+            _context.Attendances.Add(attend);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
