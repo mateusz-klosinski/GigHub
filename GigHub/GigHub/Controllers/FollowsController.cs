fc163d7cda1ff4bd5d13195f72ac90d88c89469b
Mateusz Klosinski <matix003@gmail.com>
2016-07-10 14:07:09 +0200

    Create Follows Controller and Follows Dto.

---
+++ GigHub/GigHub/Controllers/FollowsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Migrations;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Follow(FollowsDto dto)
+        {
+            var artistId = _context.Users.Single(u => u.Name == dto.ArtistName).Id;
+            var userId = User.Identity.GetUserId();
+
+
+            if (artistId == userId || _context.Follows.Any(f => f.ArtistId == artistId && f.FollowerId == userId))
+            {
+                return BadRequest("Duplikat lub odwołanie do samego siebie.");
+            }
+
+            var follow = new Follow()
+            {
+                FollowerId = userId,
+                ArtistId = artistId,
+            };
+
+            _context.Follows.Add(follow);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/GigHub/Dtos/FollowsDto.cs
@@ -0,0 +1,7 @@
+namespace GigHub.Dtos
+{
+    public class FollowsDto
+    {
+        public string ArtistName { get; set; }
+    }
+}
