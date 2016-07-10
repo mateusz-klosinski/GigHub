df75d6b489270877a3f9362e237a3eefd7b88a40
Mateusz Klosinski <matix003@gmail.com>
2016-07-10 14:58:07 +0200

    Changes in app user.

--- GigHub/GigHub/Models/ApplicationUser.cs
+++ GigHub/GigHub/Models/ApplicationUser.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,7 +15,16 @@ namespace GigHub.Models
     {
         [Required]
         [StringLength(100)]
-        public string Name { get; set; }  
+        public string Name { get; set; }
+
+        public ICollection<Follow> Followers { get; set; }
+        public ICollection<Follow> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Follow>();
+            Followees = new Collection<Follow>();
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
